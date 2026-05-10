d32324397ad0c0cc3aeafe304a2daf90e93a4e75
bt741 <brecht.van.de.sijpe@proton.me>
2026-05-10 23:27:38 +0200

    Initial try with recursion

--- src/main.rs
+++ src/main.rs
@@ -22,37 +22,32 @@ enum PieceType {
     KING,
 }
 
-
-#[derive(Debug)]
-struct Position {
-    hor: u8,
-    ver: u8,
+#[derive(Debug, Clone, PartialEq)]
+enum Color {
+    WHITE,
+    BLACK
 }
 
-impl Position {
-    fn new(hor: u8, ver: u8) -> Self {
-        Self { hor, ver }
-    }
-}
-
-#[derive(Debug)]
+#[derive(Clone)]
 struct Piece {
-    position: Position,
-    // next_moves: Vec<Position>,
     piece_type: PieceType,
+    color: Color,
 }
 
 impl Piece {
-    fn new(hor: u8, ver: u8, piece_type: PieceType) -> Self {
-        let position = Position::new(hor, ver);
+    fn new(
+        piece_type: PieceType,
+        color: Color
+    ) -> Self {
         Self {
-            position,
-            // next_moves: Position::get_next_moves(postition, type),
             piece_type,
+            color,
         }
     }
     
-    fn symbol(&self) -> char {
+    fn get_symbol(
+        &self
+    ) -> char {
         match self.piece_type {
             PieceType::PAWN => 'P',
             PieceType::TOWER => 'T',
@@ -62,38 +57,139 @@ impl Piece {
             PieceType::KING => 'K',
         }
     }
+
+    fn generate_moves(
+        &self,
+        board: &BoardState,
+        current_hor: usize,
+        current_ver: usize,
+    ) -> Vec<BoardState> {
+        let mut output: Vec<BoardState> = Vec::new();
+        
+        let mut new_ver: usize = current_ver + 1;
+        if self.color == Color::BLACK {
+            new_ver = current_ver - 1;
+        }
+
+        match self.piece_type {
+            PieceType::PAWN => {
+                let mut new_board = board.clone();
+                new_board.switch();
+                let _ = new_board.move_piece(
+                    current_hor,
+                    current_ver, 
+                    current_hor,
+                    new_ver
+                );
+                output.push(new_board);
+            },
+            _ => {},
+        }
+
+        return output;
+    }
+
+
+    fn get_color(
+        &self,
+    ) -> Color {
+        self.color.clone()
+    }
 }
 
+#[derive(Clone)]
 struct BoardState {
-    pieces: Vec<Piece>,
+    board: [Option<Piece>; 64],
+    next: Vec<BoardState>,
+    turn_color: Color,
+    bot_color: Color,
 }
 
 impl BoardState {
-    fn new(pieces: Vec<Piece>) -> Self {
+    fn new(
+        turn_color: Color,
+        bot_color: Color
+    ) -> Self {
         Self {
-            pieces
+            board: [const { None }; 64],
+            next: Vec::new(),
+            turn_color,
+            bot_color,
+        }
+    }
+
+    fn add_piece(
+        &mut self,
+        hor: usize,
+        ver: usize,
+        piece: Piece
+    ) -> () {
+        self.board[ver * 8 + hor] = Some(piece);
+    }
+
+    fn move_piece(
+        &mut self,
+        from_hor: usize,
+        from_ver: usize,
+        to_hor: usize,
+        to_ver: usize,
+    ) -> Result<(), &'static str> {
+        let from_index = from_ver * 8 + from_hor;
+        let to_index = to_ver * 8 + to_hor;
+
+        if self.board[from_index].is_none() {
+            return Err("No piece at source position");
+        }
+
+        let piece = self.board[from_index].take();
+        self.board[to_index] = piece;
+
+        Ok(())
+    }
+
+    fn pick_move(
+        &mut self,
+    ) -> Option<BoardState> {
+        self.next.pop()
+    }
+
+    fn switch(
+        &mut self,
+    ) {
+        self.turn_color = match self.turn_color {
+            Color::WHITE => Color::BLACK,
+            Color::BLACK => Color::WHITE,
+        };
+    }
+
+    fn generate_moves(&mut self) {
+        for ver in 0..8 {
+            for hor in 0..8 {
+                if let Some(piece) = &self.board[ver * 8 + hor] {
+                    if piece.get_color() == self.turn_color {
+                        let moves = piece.generate_moves(&self, hor, ver);
+                        self.next.extend(moves);
+                    }
+                }
+            }
         }
     }
 }
 
-impl fmt::Debug for BoardState {
+impl fmt::Display for BoardState {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut board = [['.'; 8]; 8];
-
-        for piece in &self.pieces {
-            let x = piece.position.hor as usize;
-            let y = piece.position.ver as usize;
-
-            board[y][x] = piece.symbol();
-        }
-
         writeln!(f, "    0 1 2 3 4 5 6 7")?;
 
-        for y in (0..8).rev() {
-            write!(f, "{} | ", y)?;
+        for ver in (0..8).rev() {
+            write!(f, "{} | ", ver)?;
 
-            for x in 0..8 {
-                write!(f, "{} ", board[y][x])?;
+            for hor in 0..8 {
+                let mut symbol = match &self.board[ver * 8 + hor] {
+                    Some(piece) => piece.get_symbol(),
+                    None => ' ',
+                };
+                write!(f, "{} ", symbol)?;
             }
 
             writeln!(f)?;
@@ -104,7 +200,6 @@ impl fmt::Debug for BoardState {
 }
 
 fn main() {
-    let mut pieces: Vec<Piece> = Vec::new();
     let piece_types = [
         PieceType::TOWER,
         PieceType::HORSE,
@@ -116,14 +211,25 @@ fn main() {
         PieceType::TOWER,
     ];
         
+    let mut board = BoardState::new(Color::BLACK, Color::WHITE);
     for i in 0..8 {
         let piece_type = &piece_types[i as usize];
-        pieces.push(Piece::new(i, 0, piece_type.clone()));
-        pieces.push(Piece::new(i, 1, PieceType::PAWN));
-        pieces.push(Piece::new(i, 6, PieceType::PAWN));
-        pieces.push(Piece::new(i, 7, piece_type.clone()));
+        board.add_piece(i, 0, Piece::new(piece_type.clone(), Color::WHITE));
+        board.add_piece(i, 1, Piece::new(PieceType::PAWN, Color::WHITE));
+        board.add_piece(i, 6, Piece::new(PieceType::PAWN, Color::BLACK));
+        board.add_piece(i, 7, Piece::new(piece_type.clone(), Color::BLACK));
     }
 
-    let board = BoardState::new(pieces);
-    println!("{:?}", board);
+  
+    let mut chessmove: Option<BoardState> = Some(board);
+    for i in 0..5 {
+        match chessmove {
+            Some(mut current_move) => {
+                println!("{}", current_move);
+                current_move.generate_moves();
+                chessmove = current_move.pick_move();
+            },
+            None => {},
+        }
+    }
 }
